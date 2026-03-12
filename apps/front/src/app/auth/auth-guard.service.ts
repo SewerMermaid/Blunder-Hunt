e432ef0253047c5a0d59a7f6ff71a4c1448f17a7
SewerMermaid <84820386+SewerMermaid@users.noreply.github.com>
2026-03-11 22:51:26 -0400

    Enhance AuthGuardService with user validation
    Refactor AuthGuardService constructor to include UsersService and enhance user validation logic.

--- apps/front/src/app/auth/auth-guard.service.ts
+++ apps/front/src/app/auth/auth-guard.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from "@angular/core";
 import { CanActivate, Router } from "@angular/router";
 import { CookieService } from "ngx-cookie";
+import { UsersService } from "../shared/http/users.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class AuthGuardService implements CanActivate {
-  constructor(private cookieService: CookieService, public router: Router) {}
+  constructor(
+    private cookieService: CookieService,
+    private usersService: UsersService,
+    public router: Router
+  ) {}
 
   async canActivate(): Promise<boolean> {
     if (!this.cookieService.get("authenticated")) {
+      await this.router.navigate([""]);
+      return false;
+    }
+
+    const user = await this.usersService.myUser();
+
+    if (!user) {
+      this.cookieService.remove("authenticated");
       await this.router.navigate([""]);
       return false;
     }
